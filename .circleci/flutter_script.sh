4fcfd42d892f58b20d420295abf3d0cce03a39a6
Neelansh <sethineelansh@gmail.com>
2019-10-12 13:13:12 +0530

    script modified again

--- .circleci/flutter_script.sh
+++ .circleci/flutter_script.sh
@@ -3,5 +3,5 @@ git clone -b master https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 flutter doctor
 yes | yes | flutter doctor --android-licenses
-cd $HOME/code/bounce_pwa
+cd $HOME/code/zubi
 flutter build apk --release
